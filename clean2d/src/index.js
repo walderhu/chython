f023cbba464442f505d685b3c9360f4fb08bf878
denis | walderhu <denis.lipatov163@gmail.com>
2024-04-12 22:26:28 +0300

    update

--- clean2d/src/index.js
+++ clean2d/src/index.js
@@ -3,18 +3,18 @@ import Parser from 'smiles-drawer/src/Parser';
 
 
 function clean2d(smiles) {
-    const drawer = new DrawerBase({});
-    const parsed = Parser.parse(smiles);
-    drawer.initDraw(parsed, 'light', false);
-    drawer.processGraph();
+  const drawer = new DrawerBase({});
+  const parsed = Parser.parse(smiles);
+  drawer.initDraw(parsed, 'light', false);
+  drawer.processGraph();
 
-    let vertices = drawer.graph.vertices;
-    let xy = Array();
-    for (let i = 0; i < vertices.length; i++) {
-      let position = vertices[i].position;
-      xy.push([position.x, position.y]);
-    }
-    return xy;
+  let vertices = drawer.graph.vertices;
+  let xy = Array();
+  for (let i = 0; i < vertices.length; i++) {
+    let position = vertices[i].position;
+    xy.push([position.x, position.y]);
+  }
+  return xy;
 }
 
 export { clean2d };
--- testing/index.py
+++ testing/index.py
@@ -1,20 +1,38 @@
-import pikachu
+from pikachu.drawing.drawing import Drawer
+from pikachu.smiles.smiles import read_smiles
 
 def clean2d(smiles):
-    # drawer = pikachu.drawing
+    drawer = Drawer(
+       structure=read_smiles(smiles_string=smiles)
+    )
+    drawer.process_structure()
+  
+    vertices = drawer.structure.graph
+    
     # drawer = DrawerBase({})
     # parsed = Parser.parse(smiles)
-
     # drawer.initDraw(parsed, 'light', False)
     # drawer.processGraph()
-    # vertices = drawer.graph.vertices
 
-
+    vertices = drawer.graph.vertices
     xy = list()
     for i in range(len(vertices)):
       position = vertices[i].position
       xy.append([position.x, position.y])
+# OR
+    # xy = [[point.position.x, point.position.y] for point in vertices]
+
 
     return xy
 
 __all__ = ['clean2d']
+
+"""создаем экземпляр класса Drawer (аналог DrawerBase), 
+в который передаем параметр smiles через функцию read_smiles, 
+которая  парсит строку на соответствие стандарту
+и в случае успеха возвращает объект класса Structure.
+второй аргумент конструктора это класс Options, который по умолчанию 
+устанавливает отрисовку цветом white.
+process_structure является аналогом processGraph в js"""
+
+
