0f358588cda26e753518220822646907b572341b
denis lipatov <denis.lipatov163@gmail.com>
2024-04-29 22:49:46 +0300

    a

--- unit.py
+++ unit.py
@@ -1,27 +1,51 @@
 import unittest
 from chython import smiles
 
+import time
+import sys
+database_smiles = 'smiles_data.txt'
+
+def ProgressBar(Total, Progress, BarLength=20, ProgressIcon="#", BarIcon="-"):
+    try:
+        if BarLength < 1:
+            BarLength = 20
+        Status = ""
+        Progress = float(Progress) / float(Total)
+        if Progress >= 1.:
+            Progress = 1
+            Status = "\r\n"
+        Block = int(round(BarLength * Progress))
+        Bar = "[{}] {:.1f}% {}".format(ProgressIcon * Block + BarIcon * (BarLength - Block), Progress * 100, Status)
+        return Bar
+    except:
+        return "ERROR"
+
+def ShowBar(Bar):
+    sys.stdout.write(Bar)
+    sys.stdout.flush()
 
 class TestAddFunction(unittest.TestCase):
-
+   
     def test(self):
-        lst = [
-            'CCCCCCCCCC(=O)N[C@@H](CC1=CNC2=CC=CC=C21)C(=O)N[C@@H](CC(=O)N)C(=O)N[C@@H](CC(=O)O)C(=O)N[C@H]3[C@H](OC(=O)[C@@H](NC(=O)[C@@H](NC(=O)[C@H](NC(=O)CNC(=O)[C@@H](NC(=O)[C@H](NC(=O)[C@@H](NC(=O)[C@@H](NC(=O)CNC3=O)CCCN)CC(=O)O)C)CC(=O)O)CO)[C@H](C)CC(=O)O)CC(=O)C4=CC=CC=C4N)C',
-            'CC=O',
-            '[Na+].CN(C)c2ccc(/N=N/c1ccc(cc1)S([O-])(=O)=O)cc2',
-            'CC=CC'#,
-            # 'C1CCCCC1', # Циклогексан
-            # 'C1CCCCC1C1CCCCC1', # Циклогексадиен
-            # 'C1CCCCC1C1CCCCC1C1CCCCC1', # Циклогексатриен
-            # 'c1ccccc1', # Бензол
-            # 'c1ccccn1', # Пиридин
-            # 'c1cccc2c1cccc2', # Фуран
-            # 'C1=CC=CC=C1C=O', # Бензол с формальдегидной группой
-                  ]
-        for smile in lst:
-            m = smiles(smile)
-            m.clean2d()
-
+        with open(database_smiles, 'r') as smiles_file, open('errors.txt', 'a') as errors_file:
+            lines = smiles_file.readlines()
+            Runs = len(lines)
+            for i, line in enumerate(lines):
+                smile = line
+                start_time = time.time() 
+                try:
+                    m = smiles(smile)
+                    m.clean2d()
+                except Exception as e:
+                    errors_file.write(f"{smile} - {str(e)}\n")
+                else:
+                    elapsed_time = time.time() - start_time
+                    if elapsed_time > 5:
+                        errors_file.write(f"{smile} - Processing took more than 5 seconds\n")
+                finally:
+                    progressBar = "\rProgress: " + ProgressBar(Runs, i + 1, BarLength=20, ProgressIcon="#", BarIcon="-")
+                ShowBar(progressBar)
+        print('\nDone.')
 
 if __name__ == '__main__':
     unittest.main()
