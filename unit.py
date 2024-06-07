f9ef07c42a19c3e9d7641240a6d69dbe91b43284
denis | walderhu <denis.lipatov163@gmail.com>
2024-06-07 13:11:15 +0300

    added hydrogen smiles formula

---
+++ unit.py
@@ -0,0 +1,11 @@
+import unittest
+# from chython import smiles
+# import time
+# import sys
+from testing import main
+class TestAddFunction(unittest.TestCase):
+    def test(self):
+        main()
+
+if __name__ == '__main__':
+    unittest.main()
