e663c378a49f3d4015792fc3c7e35238046a4ccb
denis | walderhu <denis.lipatov163@gmail.com>
2024-06-07 14:33:31 +0300

    пока вернулся обратно

--- chython/algorithms/calculate2d/drawing/drawing.py
+++ chython/algorithms/calculate2d/drawing/drawing.py
@@ -1101,117 +1101,54 @@ class Drawer:
                 closest_atom = atom_2
         return closest_atom
     
-    # def hide_hydrogens(self) -> None: оригинал
-    #     hidden: List[Atom] = []
-    #     exposed: List[Atom] = []
-    #     self.structure.refresh_structure()
-    #     for atom in self.structure.graph:
-    #         if atom.type != 'H':
-    #             continue
-    #         elif atom.charge != 0:
-    #             continue
-    #         neighbour = atom.neighbours[0]
-    #         neighbour.draw.has_hydrogen = True
-    #         atom.draw.is_drawn = False
-    #         hidden.append(atom)
-    #         if len(neighbour.draw.rings) < 2 and neighbour.draw.bridged_ring is None and \
-    #                 neighbour.draw.bridged_ring is not None and len(neighbour.draw.original_rings) < 2:
-    #             atom.draw.is_drawn = False
-    #             neighbour.draw.has_hydrogen = True
-    #             hidden.append(atom)
-    #         else:
-    #             exposed.append(atom)
-    #     for atom in self.structure.graph:
-    #         atom.set_drawn_neighbours()
-    #         if atom.type == 'O':
-    #             pass
-    #     self.drawn_bonds = []
-    #     for bond_nr, bond in self.structure.bonds.items():
-    #         if bond.atom_1.draw.is_drawn and bond.atom_2.draw.is_drawn:
-    #             self.drawn_bonds.append(bond)
-    #     self.drawn_atoms = self.structure.get_drawn_atoms()
-
-    # def hide_hydrogens(self) -> None:
-    #     hidden: List[Atom] = []
-    #     exposed: List[Atom] = []
-    #     self.structure.refresh_structure()
-    #     for atom in self.structure.graph:
-    #         if atom.type!= 'H':
-    #             continue
-    #         elif atom.charge!= 0:
-    #             continue
-    #         neighbour = atom.neighbours[0]
-    #         neighbour.draw.has_hydrogen = True
-    #         atom.draw.is_drawn = False
-    #         hidden.append(atom)
-    #         # Adjusted condition to remove the stereo center check
-    #         if len(neighbour.draw.rings) < 2 and neighbour.draw.bridged_ring is None or \
-    #         (neighbour.draw.bridged_ring is not None and len(neighbour.draw.original_rings) < 2):
-    #             atom.draw.is_drawn = False
-    #             neighbour.draw.has_hydrogen = True
-    #             hidden.append(atom)
-    #         else:
-    #             exposed.append(atom)
-    #     for atom in self.structure.graph:
-    #         atom.set_drawn_neighbours()
-    #         if atom.type == 'O':
-    #             pass
-    #     self.drawn_bonds = []
-    #     for bond_nr, bond in self.structure.bonds.items():
-    #         if bond.atom_1.draw.is_drawn and bond.atom_2.draw.is_drawn:
-    #             self.drawn_bonds.append(bond)
-    #     self.drawn_atoms = self.structure.get_drawn_atoms()
-
-# initHydrogens() {
-#     if (!this.opts.explicitHydrogens)
-#         for (var t = 0; t < this.graph.vertices.length; t++) {
-#             let e = this.graph.vertices[t];
-#             if ("H" !== e.value.element) continue;
-#             let i = this.graph.vertices[e.neighbours[0]];
-#             i.value.hasHydrogen = !0, (!i.value.isStereoCenter || i.value.rings.length < 2 && !i.value.bridgedRing || i.value.bridgedRing && i.value.originalRings.length < 2) && (e.value.isDrawn = !1)
-#         }
-# }
-
+    def hide_hydrogens(self) -> None: 
+        hidden: List[Atom] = []
+        exposed: List[Atom] = []
+        self.structure.refresh_structure()
+        for atom in self.structure.graph:
+            if atom.type != 'H':
+                continue
+            elif atom.charge != 0:
+                continue
+            neighbour = atom.neighbours[0]
+            neighbour.draw.has_hydrogen = True
+            atom.draw.is_drawn = False
+            hidden.append(atom)
+            if len(neighbour.draw.rings) < 2 and neighbour.draw.bridged_ring is None and \
+                    neighbour.draw.bridged_ring is not None and len(neighbour.draw.original_rings) < 2:
+                atom.draw.is_drawn = False
+                neighbour.draw.has_hydrogen = True
+                hidden.append(atom)
+            else:
+                exposed.append(atom)
+        for atom in self.structure.graph:
+            atom.set_drawn_neighbours()
+            if atom.type == 'O':
+                pass
+        self.drawn_bonds = []
+        for bond_nr, bond in self.structure.bonds.items():
+            if bond.atom_1.draw.is_drawn and bond.atom_2.draw.is_drawn:
+                self.drawn_bonds.append(bond)
+        self.drawn_atoms = self.structure.get_drawn_atoms()
 
     # def hide_hydrogens(self) -> None:
     #     if not self.options.draw_hydrogens:
     #         for atom in self.structure.graph:
-    #             if atom.
-                
     #             if atom.type!= 'H':
     #                 continue
-
+    #             # Hydrogens should have only one neighbour, so just take the first
+    #             # Also set has_hydrogen true on connected atom
     #             neighbour = atom.neighbours[0]
     #             neighbour.draw.has_hydrogen = True
-    #             atom.draw.is_drawn = False
-    #             if len(neighbour.draw.rings) < 2 and neighbour.draw.bridged_ring is None or \
-    #             (neighbour.draw.bridged_ring is not None and len(neighbour.draw.rings) < 2):
+
+    #             # Check if the neighbour is not a stereocenter or is part of less than 2 rings or is a bridged ring with less than 2 original rings
+    #             if not neighbour._adjacent_to_stereobond or len(neighbour.draw.rings) < 2 and neighbour.draw.bridged_ring is None or \
+    #                     (neighbour.draw.bridged_ring is not None and len(neighbour.draw.original_rings) < 2):
     #                 atom.draw.is_drawn = False
     #             else:
-    #                 atom.draw.is_drawn = True 
+    #                 atom.draw.is_drawn = True
+
     #         self.structure.refresh_structure()
     #         self.drawn_atoms = [atom for atom in self.structure.graph if atom.draw.is_drawn]
-    #         self.drawn_bonds = [bond for bond in self.structure.bonds.values()\
-    #                             if bond.atom_1.draw.is_drawn and bond.atom_2.draw.is_drawn]
-
-    def hide_hydrogens(self) -> None:
-        if not self.options.draw_hydrogens:
-            for atom in self.structure.graph:
-                if atom.type!= 'H':
-                    continue
-                # Hydrogens should have only one neighbour, so just take the first
-                # Also set has_hydrogen true on connected atom
-                neighbour = atom.neighbours[0]
-                neighbour.draw.has_hydrogen = True
-
-                # Check if the neighbour is not a stereocenter or is part of less than 2 rings or is a bridged ring with less than 2 original rings
-                if not neighbour._adjacent_to_stereobond or len(neighbour.draw.rings) < 2 and neighbour.draw.bridged_ring is None or \
-                        (neighbour.draw.bridged_ring is not None and len(neighbour.draw.original_rings) < 2):
-                    atom.draw.is_drawn = False
-                else:
-                    atom.draw.is_drawn = True
-
-            self.structure.refresh_structure()
-            self.drawn_atoms = [atom for atom in self.structure.graph if atom.draw.is_drawn]
-            self.drawn_bonds = [bond for bond in self.structure.bonds.values()
-                            if bond.atom_1.draw.is_drawn and bond.atom_2.draw.is_drawn]
+    #         self.drawn_bonds = [bond for bond in self.structure.bonds.values()
+    #                         if bond.atom_1.draw.is_drawn and bond.atom_2.draw.is_drawn]
